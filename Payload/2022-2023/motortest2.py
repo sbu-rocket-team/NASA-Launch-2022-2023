1a3168e06137bb1347cd6b8c64edce10190829bb
ethan-carr <ethan@ethancarr.dev>
2023-02-07 16:02:04 -0500

    Cleaned up motor bits, removed parameter overload

--- Payload/2022-2023/motortest2.py
+++ Payload/2022-2023/motortest2.py
@@ -9,6 +9,7 @@ setup_gpio.setup()
 rot = [pinout.ROT_ENABLE, pinout.ROT_PHASE]
 
 #mc.run_test(rot)
-mc.run_smoothstart(rot)
+#mc.run_smoothstart(pinout.ROT_ENABLE, pinout.ROT_PHASE)
+mc.run_backforth(pinout.ROT_ENABLE, pinout.ROT_PHASE)
 sleep(2)
 mc.off(pinout.ROT_ENABLE, pinout.ROT_PHASE)
