011e48a8dfd99c8eac6a594d76d2b7959bd692ee
ethan-carr <ethan@ethancarr.dev>
2023-03-01 20:21:49 -0500

    Make the camera stuff work

--- Payload/NASAcode/tools/cam_functions.py
+++ Payload/NASAcode/tools/cam_functions.py
@@ -9,15 +9,23 @@ from picamera2 import Picamera2 as pc2
 # https://docs.arducam.com/Raspberry-Pi-Camera/Native-camera/PiCamera2-User-Guide/
 # https://datasheets.raspberrypi.com/camera/picamera2-manual.pdf    < Appendix B, Configuration parameters.
 
+pcam = 0
+
 def initializeCam(res = (1920,1080)):
+    global pcam
     pcam = pc2()
     pcam_config = pcam.create_still_configuration(main={"size": res})
     pcam.configure(pcam_config)
+    time.sleep(2)
     return pcam
 
-def takePic(camera, imgName, directory = os.path.dirname(os.path.abspath(__file__))):
-    camera.start()
-    time.sleep(2)
+def takePic(imgName, directory = os.path.dirname(os.path.abspath(__file__))):
+    pcam.start()
+    time.sleep(1)
     os.chdir(directory)
-    camera.capture_file(imgName)
-    camera.close() # Needs to close as stills configuration only allows one frame buffer allocated
+    pcam.capture_file(imgName)
+    pcam.close() # Needs to close as stills configuration only allows one frame buffer allocated
+
+def clear():
+    global pcam
+    pcam = 0
